93b8d9db64b21f838451774c0eeae33c3311e354
Meric BAL <MB@Meric-MacBook-Air.local>
2016-08-29 14:59:53 -0700

    Complete!

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,24 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |s|
+      return s if s.brand == soda_brand
+      return nil if s.brand != soda_brand
+    end
   end
 
   def sell(soda_brand)
+    @sodas.each do |s|
+      if s.brand == soda_brand
+        @cash += s.price
+        @sodas.delete(s)
+      end
+    end
+      nil
   end
 
 end
